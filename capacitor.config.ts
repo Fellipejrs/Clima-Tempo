ecf481566f89f13c0cc8927dc918c1a2ac37b66d
Fellipe Rocha <155785126+Fellipejrs@users.noreply.github.com>
2024-09-09 11:53:58 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'previsao-tempo',
+  webDir: 'www'
+};
+
+export default config;
