95904e778757967e32ee15a741295b567b8451ed
Fellipe Rocha <155785126+Fellipejrs@users.noreply.github.com>
2024-09-12 09:31:07 -0300

    Subindo o projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  inputCidade: string = '';
+  cidadeNome: string | null = null;
+  graus: number | null = null;
+  clima: string | null = null;
+  icone: string | null = null;
+  tempMax: number | null = null;
+  tempMin: number | null = null;
+
+  constructor() { }
+
+  showScreen(dados: any) {
+    this.cidadeNome = dados.name;
+    this.graus = Math.floor(dados.main.temp - 273.15);
+    this.tempMax = Math.floor(dados.main.temp_max - 273.5);
+    this.tempMin = Math.floor(dados.main.temp_min - 273.5);
+
+    this.clima = dados.weather[0].description;
+    this.icone = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+
+    if(this.graus >= 30){
+      alert('Se prepare para o calor');
+    } 
+    if(this.graus <= 15){
+      alert("Pegue seu bombojaco. E sua toca porque faz frio em " + (this.cidadeNome));
+    }
+
+    
+
+    console.log(dados);
+  }
+
+  async enderecos(cidade: string) {
+    const key = '6d42f2bc27378e38bc8e995c1cfb7703';
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`).then(resposta => resposta.json());
+
+    this.showScreen(dados);
+  }
+
+  buscar() {
+    const cidade = this.inputCidade
+
+    this.enderecos(cidade);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
